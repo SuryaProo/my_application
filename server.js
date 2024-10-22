f240bb50dd68bd7fab3e297ccddd034fe42f9ddd
root <root@ip-172-31-4-132.ap-south-1.compute.internal>
2024-10-22 11:37:42 +0000

    add Dockerfile, docker-compose, edit server.js

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+   host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_NAME || "enquiryform",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to database");
+});
+
+app.get("/", (req, res) => {
+  return res.send("fetch data from users");
+});
+
+app.post("/api/form", (req, res) => {
+  const { name, email, number } = req.body;
+  const sql = "INSERT INTO users (name, email, number) VALUES (?, ?, ?)";
+  db.query(sql, [name, email, number], (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send("User added successfully");
+    }
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  const sql = "SELECT * FROM users";
+  db.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
